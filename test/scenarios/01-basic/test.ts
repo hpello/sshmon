0e51b2efc2bb7c8ffe08571438b18deca4e39e90
Hippolyte Pello <hpello@gmail.com>
2023-03-13 20:22:55 +0100

    Test: Migrate to typescript and improve docker.

--- test/scenarios/01-basic/test.ts
+++ test/scenarios/01-basic/test.ts
@@ -1,4 +1,4 @@
-import { request } from 'axios'
+import axios from 'axios'
 import { assert } from 'chai'
 
 const REQUEST_TIMEOUT = 5000
@@ -6,14 +6,14 @@ const REQUEST_TIMEOUT = 5000
 describe('sshmon', () => {
   it('access local forwarded service', async () => {
     const url = 'http://localhost:1234/hello'
-    const response = await request({ url, timeout: REQUEST_TIMEOUT })
+    const response = await axios.request({ url, timeout: REQUEST_TIMEOUT })
     assert.equal(response.status, 200)
     assert.equal(response.data, 'Hello from server\n')
   })
 
   it('access http forwarded service', async () => {
     const url = 'http://localhost:8377/proxy/server/http-forwarding/hello'
-    const response = await request({ url, timeout: REQUEST_TIMEOUT })
+    const response = await axios.request({ url, timeout: REQUEST_TIMEOUT })
     assert.equal(response.status, 200)
     assert.equal(response.data, 'Hello from server\n')
   })
