d8c378b3f573edb5109b7b9e5d70df2f1e2372d5
Hippolyte Pello <hpello@gmail.com>
2023-03-17 10:49:05 +0100

    Globa: Split lint and prettier script.

--- server/src/log/middleware.ts
+++ server/src/log/middleware.ts
@@ -3,7 +3,7 @@ import { Action, Dispatch, Middleware } from '../types/redux'
 
 const log = createLogger('redux')
 
-// @ts-ignore FIXME hpello https://github.com/gaearon/redux-thunk/issues/82
+// @ts-expect-error FIXME hpello https://github.com/gaearon/redux-thunk/issues/82
 export const logMiddleware: Middleware =
   () => (next: Dispatch) => (action: Action) => {
     if (action.type !== 'SYSTEM_ADD_STATS') {
